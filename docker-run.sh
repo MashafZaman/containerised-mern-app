46e0429f8d73af5af852465504503a11bf327fe0
Mas'haf Uz Zaman <94897001+MashafZaman@users.noreply.github.com>
2023-02-12 15:34:46 +0500

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -8,7 +8,23 @@ git checkout -b Containerised
 
 touch Dockerfile
 
-printf "FROM node:lts-alpine AS base \nWORKDIR usr/local/app \n\nFROM base AS dev \nFROM base AS react-build \nCOPY client/package.json client/yarn.lock ./ \nRUN yarn install \nCOPY client/public ./public \nCOPY client/src ./src \nRUN yarn build \n\nFROM base \nCOPY backend\package.json backend/yarn.lock ./ \nRUN yarn install \n COPY backend/src ./src \nCOPY --from=react-build /usr/local/app/build/ usr/local/app/src/static \nCMD [\"node\", \"src/index.js\"]\n"
+printf 
+"FROM node:lts-alpine AS base \n
+WORKDIR usr/local/app \n
+\n
+FROM base AS dev \n
+FROM base AS react-build \n
+COPY client/package.json client/yarn.lock ./ \n
+RUN yarn install \n
+COPY client/public ./public \n
+COPY client/src ./src \n
+RUN yarn build \n
+\n
+FROM base \n
+COPY backend\package.json backend/yarn.lock ./ \nRUN yarn install \n COPY backend/src ./src \n
+COPY --from=react-build /usr/local/app/build/ usr/local/app/src/static \n
+CMD [\"node\", \"src/index.js\"]\n" 
+> Dockerfile
 
 touch .dockerfile
 
@@ -16,7 +32,10 @@ printf "node_modules \nbuild\n" > .dockerignore
 
 touch docker-compose.yml
 
-printf "services:\n  mongo:\n    image: mongo\n    volumes:\n      - mongo-data:/data/db\n  backend:\n    build:\n      context: ./\n      target: development\n    volumes:\n      - ./backend:/usr/local/app\n      - ~/.yarn:/root/.yarn\n    ports:\n      - 4000:4000\n    environment:\n      MONGO_HOST: mongo\n  client:\n    build:\n      context: ./\n      target: development\n    volumes:\n      - ./client:/usr/local/app\n      - ~/.yarn:/root/.yarn\n    ports:\n      - 3000:3000\n  mongo-express:\n    image: mongo-express \n    ports:\n      - 8081:8081 \nvolumes: \n  mongo-data:\n" > docker-compose.yml
+printf 
+"services:\n
+mongo:\n    image: mongo\n    volumes:\n      - mongo-data:/data/db\n  backend:\n    build:\n      context: ./\n      target: development\n    volumes:\n      - ./backend:/usr/local/app\n      - ~/.yarn:/root/.yarn\n    ports:\n      - 4000:4000\n    environment:\n      MONGO_HOST: mongo\n  client:\n    build:\n      context: ./\n      target: development\n    volumes:\n      - ./client:/usr/local/app\n      - ~/.yarn:/root/.yarn\n    ports:\n      - 3000:3000\n  mongo-express:\n    image: mongo-express \n    ports:\n      - 8081:8081 \nvolumes: \n  mongo-data:\n" 
+> docker-compose.yml
 
 docker run --network=workshop --network-alias=mongo mongo
 
